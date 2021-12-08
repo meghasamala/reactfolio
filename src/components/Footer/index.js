ea62307f09e94f42bd0bc9a505e80ed59f75df6d
megha <msamala21@gmail.com>
2021-12-07 22:37:01 -0600

    header and footer setup

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer>
+            <h6>made by Megha</h6>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../App.css';
+
+function Header() {
+    return(
+      <header className="header">
+        <h1>Megha Samala</h1>
+      </header>
+    )
+}
+
+export default Header;
